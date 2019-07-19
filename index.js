cbf909d5b7146a862e340100e073024c45267657
Quang Dang <quanghd96@gmail.com>
2019-07-19 11:27:33 +0700

    add express server

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
 const gitDirectory = "/Volumes/DATA/test-git-auto";
-const isExist = fs.existsSync(gitDirectory);
-if (!isExist) fs.mkdirSync(gitDirectory);
-const git = require("simple-git/promise")(gitDirectory);
-git.checkIsRepo().then(isRepo => (isRepo ? pullRepo(git) : cloneRepo(git)));
+const express = require("express");
+var cors = require("cors");
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.get("/update", async (req, res) => {
+  try {
+    const isExist = fs.existsSync(gitDirectory);
+    if (!isExist) fs.mkdirSync(gitDirectory);
+    const git = require("simple-git/promise")(gitDirectory);
+    const isRepo = await git.checkIsRepo();
+    isRepo ? pullRepo(git) : cloneRepo(git);
+    res.send();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 const cloneRepo = git => {
   return git.clone("https://github.com/quanghd96/AndroidCalendar", ".");
 };
 const pullRepo = git => {
   return git.pull();
 };
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
